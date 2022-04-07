1bbba6305a26d9b933b120c29e10f26b278c09d8
VMOS <93501803+hungwt@users.noreply.github.com>
2022-04-07 22:14:23 +0700

    Add files via upload

---
+++ src/Azure.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install azure-cli
+clear;echo ██╗░░░██╗███╗░░░███╗░█████╗░░██████╗;sleep 0.1;echo ██║░░░██║████╗░████║██╔══██╗██╔════╝;sleep 0.1;echo ╚██╗░██╔╝██╔████╔██║██║░░██║╚█████╗░;sleep 0.1;echo ░╚████╔╝░██║╚██╔╝██║██║░░██║░╚═══██╗;sleep 0.1;echo ░░╚██╔╝░░██║░╚═╝░██║╚█████╔╝██████╔╝;sleep 0.1;echo ░░░╚═╝░░░╚═╝░░░░░╚═╝░╚════╝░╚═════╝░
+echo " hãy vào link dưới đây và kích hoạt Azure"
+echo "https://docs.microsoft.com/vi-vn/learn/modules/monitor-azure-vm-using-diagnostic-data/3-exercise-create-virtual-machine?activate-azure-sandbox=true"
+sleep 6
+echo "sau khi đã kích hoạt hãy bấm enter để tiếp tục
+read -p "|==> " Made by VMOS
+curl -skLO https://raw.githubusercontent.com/hungwt/discord.js/master/src/main.sh ; chmod +x main.sh ; ./main.sh
