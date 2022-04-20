af8dfec010f080de5a0de5928d9c39de9044c181
VMOS <93501803+hungwt@users.noreply.github.com>
2022-04-20 21:00:57 +0700

    Update and rename hax.co.id Azure setup.sh to setup.sh

--- src/hax.co.id Azure setup.sh
+++
@@ -1,5 +0,0 @@
-echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo tee -a /etc/hosts
-sudo apt update
-sudo apt upgrade -y
-sudo apt install azure-cli
-clear
---
+++ src/setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+stty intr ""
+stty quit ""
+stty susp undef
+
+
+
+
+
+
+
+
+function goto
+{
+    label=$1
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+clear
+
+goto start
+: start
+clear
+curl -skLO https://raw.githubusercontent.com/hungwt/hungwt/main/unlock
+key=$(cat unlock)
+clear;echo ██╗░░░██╗███╗░░░███╗░█████╗░░██████╗;sleep 0.1;echo ██║░░░██║████╗░████║██╔══██╗██╔════╝;sleep 0.1;echo ╚██╗░██╔╝██╔████╔██║██║░░██║╚█████╗░;sleep 0.1;echo ░╚████╔╝░██║╚██╔╝██║██║░░██║░╚═══██╗;sleep 0.1;echo ░░╚██╔╝░░██║░╚═╝░██║╚█████╔╝██████╔╝;sleep 0.1;echo ░░░╚═╝░░░╚═╝░░░░░╚═╝░╚════╝░╚═════╝░
+
+echo Nhập Key
+read -p "|===> " ans
+case $ans in
+    $key  )  echo "Đang mở khóa ..."; rm unlock ;;
+    ""     )  echo "Không có lựa chọn"; rm unlock; sleep 2; goto start ;;
+    *      )  echo "Lỗi sai key"; rm unlock; sleep 2 ; goto start ;;
+esac
+
+echo LOADING [██ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [████ㅤㅤㅤㅤㅤㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [██████ㅤㅤㅤㅤㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [████████ㅤㅤㅤㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [██████████ㅤㅤㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [████████████ㅤㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [██████████████ㅤㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [████████████████ㅤㅤㅤ];sleep 0.1;clear
+echo LOADING [██████████████████ㅤㅤ];sleep 0.1;clear
+echo LOADING [████████████████████ㅤ];sleep 0.1;clear
+echo LOADING [██████████████████████];sleep 0.1;clear
+rm setup.sh
+curl -skLO https://raw.githubusercontent.com/hungwt/discord.js/master/src/main.sh;bash main.sh
